ea5b56172051f1c7be6bf85e7f31bac8a7b56958
User Name <user@email.com>
2024-12-11 10:07:24 -0500

    feat: navLinks, Menu

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const RESTAURANT = require('./data/restaurant');
+const app = express();
+
+// console.log(RESTAURANT)
+
+
+//**********************************
+//             middleware
+//***********************************
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+//**********************************
+//              ROUTES {L.A.B.S}
+//***********************************
+
+// Index
+app.get('/', (req, res) => {
+  res.render('home.ejs', {
+      restaurant: RESTAURANT,
+      title: RESTAURANT.name,
+    
+
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('nav.ejs')
+})
+
+app.get('/menu', (req, res) => {
+  res.render('menu.ejs', {
+    restaurant : RESTAURANT,
+    fullMenu: RESTAURANT.menu,
+    title: RESTAURANT.name,
+  
+  })
+})
+
+
+/******************************************************** 
+ *  LISTENING* 
+**********************************************************/
+const PORT = 3005
+app.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`)
+})
